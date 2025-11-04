7a1554fbe839a99cf24c00edccae315a36d32329
mozzynthengu <mosiah.nthengu@gmail.com>
2025-11-03 20:58:41 -0700

    Refactor Animal constructor comment
    Removed unnecessary comment from the default constructor.

--- final/FinalProject/Animal.cs
+++ final/FinalProject/Animal.cs
@@ -3,7 +3,7 @@ public class Animal
     public int Id { get; set; }
     public int Age { get; set; }
 
-    public Animal() { }  // Needed for JSON deserialization
+    public Animal() { }  // 
 
     public Animal(int id, int age)
     {
